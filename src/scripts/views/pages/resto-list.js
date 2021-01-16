888c562a3478886a887da3408a5884f1c8086ddb
Alvin Indra <alvinindrap@gmail.com>
2021-01-16 12:55:45 +0700

    style: update content and stylign card list resto

--- src/scripts/views/pages/resto-list.js
+++ src/scripts/views/pages/resto-list.js
@@ -9,14 +9,17 @@ function getRestaurants(data) {
   const resto = data.restaurants
   resto.forEach(restoItem => {
     content.innerHTML +=  
-      `<a tabindex="0" class="card-resto" href="/detail.html?id=${restoItem.id}">
-        <img src="${restoItem.pictureId}" class="card-resto__image" alt="${restoItem.name}">
+      `<div tabindex="0" class="card-resto">
+        <div class="card-cover">
+          <img class="card-cover__img" draggable="false" src="${restoItem.pictureId}" alt="${restoItem.name}">
+          <div class="card-cover__rating"><img src="./images/content/star-rating.png" alt="Star"><span>${restoItem.rating}</span></div>
+        </div>
         <div class="card-desc">
-          <h3 class="mx-3">${restoItem.name}</h3>
-          <p>
-            ${restoItem.description.slice(0,150)}
+          <h3 class="card-title mx-3">${restoItem.name}</h3>
+          <p class="card-desc__text">
+            ${restoItem.description.slice(0,200)}
           </p>
         </div>
-      </a>`
+      </div>`
   })
 }
