89d7748e722144fe374c6875c75fe69eb04fec8a
Alvin Indra <alvinindrap@gmail.com>
2021-01-20 22:56:58 +0700

    chore: add off canvas menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,8 +4,17 @@ import './views/resto-list'
 
 document.querySelector("#openNav").addEventListener("click", function () {
   document.getElementById("sideNav").style.width = "250px"
+  event.stopPropagation();
 })
 
 document.querySelector("#closeNav").addEventListener("click", function () {
   document.getElementById("sideNav").style.width = "0"
+  event.stopPropagation();
+})
+
+const mainElement = document.querySelector("main");
+ 
+mainElement.addEventListener("click", event => {
+  document.getElementById("sideNav").style.width = "0"
+  event.stopPropagation();
 })
